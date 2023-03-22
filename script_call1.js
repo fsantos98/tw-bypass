2eb8b457c93b0097c47e3978c100b4ed2b1c2c3d
fsantos98 <franciscoslb.98@hotmail.com>
2023-03-22 12:17:02 +0000

    remove useless function

--- script_call1.js
+++ script_call1.js
@@ -1,31 +1,4 @@
 let _0x54468f = window
-_0x5f3dac()
-function _0x5f3dac() {
-  const _0x9c4d94 = (function () {
-      let _0x2df30a = true
-      return function (_0x4b3fab, _0x3fea2b) {
-        const _0x39dca2 = _0x2df30a
-          ? function () {
-              if (_0x3fea2b) {
-                const _0x2d8366 = _0x3fea2b.apply(_0x4b3fab, arguments)
-                return (_0x3fea2b = null), _0x2d8366
-              }
-            }
-          : function () {}
-        return (_0x2df30a = false), _0x39dca2
-      }
-    })(),
-    _0x2119a7 = _0x9c4d94(this, function () {
-      return _0x2119a7
-        .toString()
-        .search('(((.+)+)+)+$')
-        .toString()
-        .constructor(_0x2119a7)
-        .search('(((.+)+)+)+$')
-    })
-  _0x2119a7()
-  _0x1f365f()
-}
 function _0x1f365f() {
   if (
     window.location.href.includes('screen=memo') ||
